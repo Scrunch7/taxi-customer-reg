f5c92c97a1233b1162a804bf89bc0534f6488156
Taehg <102633219+Scrunch7@users.noreply.github.com>
2022-06-19 17:34:03 +1200

    fixed reg

--- Taxi_Service_Official_2/Taxi Project/Taxi Project/CustomerRegistration.cpp
+++ Taxi_Service_Official_2/Taxi Project/Taxi Project/CustomerRegistration.cpp
@@ -1,48 +1,29 @@
-#include "Functions.h"
-
-
 void custReg(string customerFile)
 {
 	DrawLine(15);
 	cout << "\nWelcome Customer\n";
-	DrawLine(15); cout << "\n"; 
+	DrawLine(15); cout << "\n";
 	openClose(customerFile);
 	landing();
 }
 
 void openClose(string customerFile) {
-	
+	//Creates a new variable file of type output file system (ofstream)
+	fstream myFile;
 
-	//Creates a new variable myFile of type output file system (ofstream)
-	std::ofstream myFile;
-
-	//Sets myFile to a file of user defined name
+	//Sets file to a file of user defined name
 	//If no such file exists, a new one will be created
-	myFile.open("customerFile.csv");
+	myFile.open("customerFile.csv", ios::app);//write
 
 	//Determine if file has opened successfully
-	if (!myFile.is_open()) {
+	/*if (!myFile.is_open()) {
 		std::cout << "\nWARNING: Cannot open file specified\n";
-	}
+	}*/
 	std::cout << "\nFile created and opened successfully\n\n";
-
-	customerRegistration(customerFile);
-
-	//Close the file assigned to myFile
-	myFile.close();
-	if (!myFile.is_open()) {
-		std::cout << "File successfully close\n\n";
-	}
-	return;
-}
-
-void customerRegistration(string customerFile) {
-
-	ofstream file("customerFile.csv", ios::out);
 
 	Customer test;
 	cout << "Enter your full name: ";
-	cin >> test.fullName;
+	cin.ignore(); getline(cin, test.fullName);
 	cout << "Enter your Contact Number: ";
 	cin >> test.contactNumber;
 	cout << "Enter your Email: ";
@@ -51,8 +32,12 @@ void customerRegistration(string customerFile) {
 	cin >> test.streetAddress;
 
 
-	file << test.fullName << ","
+	myFile << test.fullName << ","
 		<< test.contactNumber << ","
 		<< test.email << ","
 		<< test.streetAddress << std::endl;
+
+	//Close the file assigned to myFile
+	myFile.close();
+
 }
